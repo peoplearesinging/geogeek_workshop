2abf24d90897d7324aa6c6bd6ddaa861a5b2e29c
peoplearesinging <150631996+peoplearesinging@users.noreply.github.com>
2023-11-13 14:40:21 +0100

    initial commit

---
+++ git_workshop_files/git_workshop_files/population.py
@@ -0,0 +1,10 @@
+with open("data/cities.csv", "r") as f:
+    lines = f.readlines()[1:]
+
+total = 0
+for line in lines:
+    name, population = line.split(";")
+    total += int(population)
+
+print(f"Total population: {total:,}")
+print(f"Average population: {total / len(lines):,}")
